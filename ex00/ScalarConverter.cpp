c58519b27d5e8c117aeab216e4d79d2846687da9
amblanch <177555279+blanchetamaury@users.noreply.github.com>
2026-01-12 08:53:34 +0100

    Fix: special value

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: amblanch <amblanch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 15:21:01 by amaury            #+#    #+#             */
-/*   Updated: 2026/01/12 08:42:59 by amblanch         ###   ########.fr       */
+/*   Updated: 2026/01/12 08:50:49 by amblanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,46 +38,48 @@ void    ScalarConverter::convert(std::string input) {
         input += "f";
     }
     
-    try {
-        if (input.find('.') != std::string::npos && input.find('.', input.find('.') + 1) != std::string::npos) {
-            throw std::invalid_argument("multi [.] found !");
-        }
-        if (input.find('.') == 0 || input.find('.') == input.size() - 1) {
-            throw std::invalid_argument("bad pos for character [.] !");
-        }
-        
-        if (input.find('f') != std::string::npos && input.find('f', input.find('f') + 1) != std::string::npos) {
-            throw std::invalid_argument("multi [f] found !");
-        }
-        if (input.find('f') != input.size() - 1 && input.find('f') != std::string::npos) {
-            throw std::invalid_argument("bad pos for character [f] !");
-        }
-        
-        if (specialValue == false && input.size() != 1 && input[input.size() - 1] == 'f')
-            input.erase(input.size() - 1);
-        if (input.size() == 1 && !isdigit(input.c_str()[0]))
-            value = static_cast<int>(input.c_str()[0]);
-        else {
-            std::stringstream ss(input);
-            ss.precision(1);
-            ss >> value;
-            if (value > 1e6L || value < -1e6L)
-                std::cout << std::scientific << std::setprecision(1);
-            else
-                std::cout << std::fixed << std::setprecision(1);
-            if ((ss.fail() || !ss.eof()) && specialValue == false) {
-                std::cerr << "Invalid Input" << std::endl;
-                return ;
+    if (specialValue == false) {
+        try {
+            if (input.find('.') != std::string::npos && input.find('.', input.find('.') + 1) != std::string::npos) {
+                throw std::invalid_argument("multi [.] found !");
+            }
+            if (input.find('.') == 0 || input.find('.') == input.size() - 1) {
+                throw std::invalid_argument("bad pos for character [.] !");
+            }
+            
+            if (input.find('f') != std::string::npos && input.find('f', input.find('f') + 1) != std::string::npos) {
+                throw std::invalid_argument("multi [f] found !");
+            }
+            if (input.find('f') != input.size() - 1 && input.find('f') != std::string::npos) {
+                throw std::invalid_argument("bad pos for character [f] !");
+            }
+            
+            if (specialValue == false && input.size() != 1 && input[input.size() - 1] == 'f')
+                input.erase(input.size() - 1);
+            if (input.size() == 1 && !isdigit(input.c_str()[0]))
+                value = static_cast<int>(input.c_str()[0]);
+            else {
+                std::stringstream ss(input);
+                ss.precision(1);
+                ss >> value;
+                if (value > 1e6L || value < -1e6L)
+                    std::cout << std::scientific << std::setprecision(1);
+                else
+                    std::cout << std::fixed << std::setprecision(1);
+                if (ss.fail() || !ss.eof()) {
+                    std::cerr << "Invalid Input" << std::endl;
+                    return ;
+                }
             }
         }
-    }
-    catch (std::invalid_argument const& e) {
-        std::cerr << "std::invalid_argument::what(): " << "'" << e.what() << "'" << std::endl;;
-        return ;
-    }
-    catch (std::out_of_range const& e) {
-        std::cerr << "std::out_of_range::what(): " << "'" << e.what() << "'" << std::endl;
-        return ;
+        catch (std::invalid_argument const& e) {
+            std::cerr << "std::invalid_argument::what(): " << "'" << e.what() << "'" << std::endl;;
+            return ;
+        }
+        catch (std::out_of_range const& e) {
+            std::cerr << "std::out_of_range::what(): " << "'" << e.what() << "'" << std::endl;
+            return ;
+        }
     }
     
     if (value < 0 || value > 127 || specialValue)                   {std::cout << "char: impossible" << std::endl;}
